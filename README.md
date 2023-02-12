79a061d37a7b798164abe33ffc20dbdc199a45d5
Tomas Bjerre <tomas.bjerre85@gmail.com>
2023-02-12 16:36:05 +0100

    it begins

---
+++ README.md
@@ -0,0 +1,23 @@
+# Plantuml CLI
+
+[![NPM](https://img.shields.io/npm/v/plantuml-cli.svg?style=flat-square) ](https://www.npmjs.com/package/plantuml-cli)
+
+This is [Plantuml](https://github.com/plantuml/plantuml) wrapped [inside an NPM package](https://github.com/tomasbjerre/npm-java-runner). It relies only on dependencies found in NPM.
+
+See: https://github.com/plantuml/plantuml
+
+## Example - Command line
+
+It can be used as a command line tool like this:
+
+```bash
+npx plantuml-cli
+```
+
+## Command line arguments
+
+Available command line arguments `npx plantuml-cli --help`:
+
+```sh
+
+```
---
+++ release.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+version=$(node -p -e "require('./package.json').version")
+echo releasing $version
+
+rm -rf build \
+ && mkdir build \
+ && wget –q https://github.com/plantuml/plantuml/releases/download/v$version/plantuml-$version.jar -P build
+
+FILE=build/plantuml-$version.jar
+if [ -f "$FILE" ]; then
+ echo JAR downloaded ok
+else
+ echo JAR not downloaded ok
+ exit
+fi
+
+npm version prerelease --preid=alpha
+rm -f package-lock.json
+npm install \
+ && npm publish \
+ && git commit -a -m "releasing $version" \
+ && git tag $version \
+ && git push -u origin --tags \
+ && git push
